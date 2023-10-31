341fc8ecb162029cebc38026bde7d937fea2456e
omar-chaar <53823379+omar-chaar@users.noreply.github.com>
2023-10-31 17:21:41 +0400

    Restructured files

---
+++ auth.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends, FastAPI
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from starlette import status
+from typing import Annotated
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"],
+)
+
+SECRET_KEY = "verysecretkey"
+ALGORITHM = "HS256"
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/auth/token")
+
+class CreateUserRequest(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    create_user_model = Users(username=request.username, password=bcrypt_context.hash(request.password))
+    
+    db.add(create_user_model)
+    db.commit()
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import uvicorn
-from server.model import PostSchema
+from model import PostSchema
 from fastapi import FastAPI
+import auth
 
 server = FastAPI()
+server.include_router(auth.router)
 
 @server.get("/")
 def index():
